14dce872329df6f4213de0a0a8db1246d046f3ba
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-07-09 20:08:18 +0200

    preprocessor commands for portanbility

--- prompt.c
+++ prompt.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/* if we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
 
 /* declare a buffer for user input of size 2048 */
-static char input[2048];
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt){
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) -1] = '\0';
+  return cpy;
+}
+
+/* fake add_history function */
+void add_history(char* unused){}
+
+/* otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv){
 
@@ -12,14 +36,12 @@ int main(int argc, char** argv){
   /* never ending loop */
   while(1){
 
-    /* output our prompt */
-    fputs("lispy> ", stdout);
+    /* now in either case readline will be correctly defined */
+    char* input = readline("lispy> ");
+    add_history(input);
 
-    /* read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
-
-    /* echo input back to user */
-    printf("No you are a %s", input);
+    printf("No you're a %s\n", input);
+    free(input);
 
   }
 
