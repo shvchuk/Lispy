a7dd66e3387bb1b21861d55424b2a8626efe9bf0
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-07-10 06:57:23 +0200

    parsers & grammar for the language defined

--- parsing.c
+++ parsing.c
@@ -30,6 +30,24 @@ void add_history(char* unused){}
 
 int main(int argc, char** argv){
 
+  /* grammar for the language of polish notation */
+
+  /* create some parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  /* define parsers with the following language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+	    "                                                    \
+               number  : /-?[0-9]+/ ;                            \
+               operator: '+' | '-' | '*' | '/' ;                 \
+               expr    : <number> | '(' <operator> <expr>+ ')' ; \
+               lispy   : /^/ <operator> <expr>+ /$/ ;            \
+            ",
+	    Number, Operator, Lispy);
+
   /* print version and exit information */
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
@@ -46,6 +64,8 @@ int main(int argc, char** argv){
 
   }
 
+  /* undefine and delete parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 
 }
